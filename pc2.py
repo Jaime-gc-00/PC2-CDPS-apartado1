fecdbb88a6e9f4d158f8643787458f85c8d160d5
Jaime-gc-00 <78904735+Jaime-gc-00@users.noreply.github.com>
2022-01-19 15:02:10 +0100

    Update pc2.py

--- pc2.py
+++ pc2.py
@@ -17,19 +17,19 @@ os.environ['GROUP_NUMBER']="Equipo 40"
 numero_de_grupo = str(os.environ.get('GROUP_NUMBER'))
 
 call(['sudo apt-get update'], shell=True)
-call(['sudo apt-get install nano'], shell=True)
-call(['sudo apt-get install pip'], shell=True)
-call(["sudo sudo apt-get install python3-pip"], shell=True)
+call(['sudo apt-get install -y nano'], shell=True)
+call(['sudo apt-get install -y pip'], shell=True)
+call(["sudo sudo apt-get install -y python3-pip"], shell=True)
 
 #Descarga de la aplicacion 
-call(['sudo sudo apt-get install nodejs'], shell=True)
-call(['sudo sudo apt-get install npm'], shell=True)
+call(['sudo sudo apt-get install -y nodejs'], shell=True)
+call(['sudo sudo apt-get install -y npm'], shell=True)
 call(['sudo git clone https://github.com/CDPS-ETSIT/practica_creativa2.git'], shell=True)
 
 #INSTALACION DE REQUIRMENTS CON PIP
 requirements = open(cwd+"/practica_creativa2/bookinfo/src/productpage/requirements.txt", 'r')
 for line in requirements:
-    call(["pip3 install " + line ], shell=True)
+    call(["pip3 install -y " + line ], shell=True)
 
 #MODIFICA EL TITULO Y ARRANCA LA APLICACION EN EL PUERTO :9999
 call(['cp '+cwd+'/practica_creativa2/bookinfo/src/productpage/templates/productpage.html '+cwd+'/practica_creativa2/bookinfo/src/productpage/templates/productpage2.html'], shell=True)
